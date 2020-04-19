46606035d684f4d5e424543b4c101a31e8f9633b
Taeho Yoo <tyoo002@wch136-09.cs.ucr.edu>
2020-04-19 16:15:43 -0700

    test part 2

--- Lab4/turnin/tyoo002_lab4_part2.c
+++ Lab4/turnin/tyoo002_lab4_part2.c
@@ -12,37 +12,45 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States {Start, Off, On} state;
+enum States {Start, Off_Release, On_Press, On_Release, Off_Press} state;
 
 void Tick() {
-	switch(state) {
-		case Start:
-			PORTC = 0x07;
-			state = Off;
+        switch(state) {
+                case Start:
+                        PORTC = 0x07;
+                        state = Off_Release;
+                        break;
+                case Off_Release:
+			if(PINA != 0x00) {
+				state = On_Press;
+			}
+			else { 
+				state = Off_Release;
+			}
 			break;
-		case Off:
-			if(PINA != 0x00) { state = On; }
+		case On_Press:
+			if(!PINA) { 
+				state = On_Release;
+			}
+			else {
+				state = On_Press;
+			}
 			break;
-		case On:
-			state = Off;
-			break;
-		default:
-			break;
-	}
-	switch(state) {
-		case On:
-			if(PINA == 0x01) {
+                default:
+                        break;
+        }	
+	switch(state) {	
+		case On_Press:
+			if(PINA == 0x01) { 
 				if(PORTC < 9) { PORTC++; }
 			}
 			else if(PINA == 0x02) {
-				if(PORTC > 0) { PORTC--; } 
+				if(PORTC > 0) { PORTC--; }
 			}
-			else { PORTC = 0; }
+			else if(PINA == 0x03) { PORTC = 0; }
 			break;
-		default:
-			break;
-	}
-		
+	}	
+
 }
 int main(void) {
     /* Insert DDR and PORT initializations */
@@ -52,7 +60,8 @@ int main(void) {
     /* Insert your solution below */
     state = Start;
     while (1) {
-	Tick();
+        Tick();
     }
     return 0;
 }
+
